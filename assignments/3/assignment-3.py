389f3b4bc6acbea91e70a27b4eef3e3afb5e9033
v0xnihili <douwedb@gmail.com>
2019-05-17 10:33:59 +0200

    Now it fully works. I only need to add new graphs + parts d and e from the exercise

--- assignments/3/assignment-3.py
+++ assignments/3/assignment-3.py
@@ -6,16 +6,28 @@ import math
 
 import isa
 
-from reentry import ReEntry
+from reentry import ReEntry, PathTravelled
 from planet import Planet
 from spacecraft import Spacecraft
 
 import matplotlib.pyplot as plotter
 
 # ------------------------------------------------------
+# Terminal Setup
+# ------------------------------------------------------
+
+print("")
+print("=============================")
+print("| V0X's Re-entry Calculator |")
+print("=============================")
+print("\nLast edited on 17 May 2019")
+print("\n-----------------------------\n")
+
+# ------------------------------------------------------
 # Define constants
 # ------------------------------------------------------
 
+# All of these values where given in the exercise
 fSpacecraftMassKg = 2662.8 * 0.45359237
 fReferenceArea = 2.81
 fDragCoefficient = 1.60
@@ -25,18 +37,32 @@ fEarthRadius = 6371 * 1000
 
 fInitialAltitude = 325 * 1000 * 0.3048
 fInitialVelocity = 22.5 * 1000 * 0.3048
-fFlightPathDeg = 1.5
+fFlightPathDeg = -1.5
 
+# Create spacecraft and planet objects with their respective properties
 currentSpacecraft = Spacecraft(fSpacecraftMassKg, fReferenceArea, fDragCoefficient)
 currentPlanet = Planet(fEarthMass, fEarthRadius)
 
 # ------------------------------------------------------
-# Run simulation
+# Simulate the whole thing
 # ------------------------------------------------------
 
+# Create, run and save the simulation
 currentSimulation = ReEntry(currentSpacecraft, currentPlanet, fInitialAltitude, fInitialVelocity, fFlightPathDeg)
+arrDatapointsFromSimulation = currentSimulation.simulate(0.1)
+travelledPath = PathTravelled(arrDatapointsFromSimulation)
 
-arrDatapointsFromSimulation = currentSimulation.simulate(0.2)
+# ------------------------------------------------------
+# Write required output to console
+# ------------------------------------------------------
+
+velocity2DAt32Km = travelledPath.getvelocityataltitude(32*1000)
+velocity2DAt11Km = travelledPath.getvelocityataltitude(11*1000)
+velocity2DAt0Km = travelledPath.getvelocityataltitude(0)
+
+print("> Velocity at 32,000 [m]:\n\n" + "x:       " + str(round(velocity2DAt32Km.x, 1)) + " [m/s]\ny:       " + str(round(velocity2DAt32Km.y, 1)) + " [m/s]\nTotal:   " + str(round(velocity2DAt32Km.length(), 1))  + " [m/s]\n")
+print("> Velocity at 11,000 [m]:\n\n" + "x:       " + str(round(velocity2DAt11Km.x, 1)) + " [m/s]\ny:       " + str(round(velocity2DAt11Km.y, 1)) + " [m/s]\nTotal:   " + str(round(velocity2DAt11Km.length(), 1))  + " [m/s]\n")
+print("> Velocity at 0 [m]:\n\n"      + "x:       " + str(round(velocity2DAt0Km.x, 1))  + " [m/s]\ny:       " + str(round(velocity2DAt0Km.y, 1))  + " [m/s]\nTotal:   " + str(round(velocity2DAt0Km.length(), 1))   + " [m/s]\n")
 
 # ------------------------------------------------------
 # Display plot of results
@@ -47,13 +73,13 @@ arrY = []
 
 for dataPoint in arrDatapointsFromSimulation:
     arrX.append(dataPoint[3])
-    arrY.append(dataPoint[2].x)
-
-'''
-for alt in range(0, 100*1000):
-    arrY.append(alt)
-    arrX.append(isa.calculate(alt)[0])
-'''
+    arrY.append(dataPoint[2].length())
 
 plotter.plot(arrX, arrY)
 plotter.show()
+
+
+# x: alt in kft, y: speed in kft/s
+# time on x in sec, g force of y-accel on y axis
+# range (hor distance from impact point ) on x-axis, altitude in kft on y-axis
+# time in minutes on x-axis, altitude in kft on y-axis
--- assignments/3/reentry.py
+++ assignments/3/reentry.py
@@ -17,29 +17,29 @@ class ReEntry:
         fCurrentAltitude = self.fInitialAltitude
         fCurrentFlightPath = self.fInitialFlightPath
         currentVelocity2D = Vector2D(self.fInitialVelocity * math.cos(math.radians(fCurrentFlightPath)), self.fInitialVelocity * math.sin(math.radians(fCurrentFlightPath)))
-        
+
         arrDataPoints = []
 
         fCurrentTravelTime = 0
 
-        while fCurrentAltitude > 15*1000:
+        while fCurrentAltitude > 0:
             fCurrentDensity = isa.calculate(fCurrentAltitude)[2] # Density is the second column of the return contents of the ISA
 
             fGravitationalForce = self.getgravitationalforce(fCurrentAltitude)
             fDragForce = self.getdragforce(currentVelocity2D.length(), fCurrentDensity)
 
-            fResultantX = -fDragForce * math.cos(math.radians(fCurrentFlightPath)) # -fDragForce #* math.cos(math.radians(fCurrentFlightPath)) # To the right is positive
-            fResultantY = fGravitationalForce - fDragForce * math.sin(math.radians(fCurrentFlightPath)) # Downwards is positive
+            fResultantX = - fDragForce * math.cos(math.radians(fCurrentFlightPath))
+            fResultantY = - fDragForce * math.sin(math.radians(fCurrentFlightPath)) - fGravitationalForce
 
             currentAcceleration2D = Vector2D(fResultantX / self.objSpacecraft.mass, fResultantY / self.objSpacecraft.mass)
             currentVelocity2D = Vector2D(currentVelocity2D.x + currentAcceleration2D.x * timeInterval, currentVelocity2D.y + currentAcceleration2D.y * timeInterval)
-            
-            fCurrentAltitude -= currentVelocity2D.y * timeInterval    # -= because re-entry means the spacecraft is going down
-            fCurrentFlightPath = math.degrees(math.tanh(currentVelocity2D.y / currentVelocity2D.x))
+
+            fCurrentAltitude += currentVelocity2D.y * timeInterval
+            fCurrentFlightPath = math.degrees(math.atan(currentVelocity2D.y / currentVelocity2D.x))
 
             fCurrentTravelTime += timeInterval
 
-            arrDataPoints.append([fCurrentTravelTime, currentAcceleration2D, currentVelocity2D, fCurrentAltitude, fCurrentFlightPath, fCurrentDensity])
+            arrDataPoints.append([fCurrentTravelTime, currentAcceleration2D, currentVelocity2D, fCurrentAltitude, fCurrentFlightPath])
 
         return arrDataPoints
 
@@ -48,3 +48,19 @@ class ReEntry:
 
     def getdragforce (self, velocity, density):
         return 0.5 * self.objSpacecraft.dragCoefficient * density * math.pow(velocity, 2) * self.objSpacecraft.frontalArea
+
+class PathTravelled:
+    def __init__ (self, dataPoints): # Should be in the same format as what ReEntry returns
+        self.arrDataPoints = dataPoints
+
+    def getvelocityataltitude (self, altitude):
+        fSmallestSquaredDistanceToAltitude = math.pow((self.arrDataPoints[0][3] - altitude), 2)
+        velocity2DAtAltitude = Vector2D(0, 0)
+
+        for dataPoint in self.arrDataPoints:
+            fSquaredAltitudeDistance = math.pow((dataPoint[3] - altitude), 2)
+            if fSquaredAltitudeDistance < fSmallestSquaredDistanceToAltitude:
+                fSmallestSquaredDistanceToAltitude = fSquaredAltitudeDistance
+                velocity2DAtAltitude = dataPoint[2]
+
+        return velocity2DAtAltitude
